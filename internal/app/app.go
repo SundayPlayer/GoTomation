ad32c9178763339ba5067c312d7624b78e0f0d05
Baptiste Vadé <baptiste.vade@hotmail.fr>
2020-04-06 16:46:03 +0200

    App: reorganisation of the app file

--- internal/app/app.go
+++ internal/app/app.go
@@ -7,20 +7,24 @@ import (
 	"time"
 )
 
-var (
-	logger *log.Logger
-)
+type App struct {
+	Config config.GlobalConfig
+	Log    *log.Logger
+}
 
-func Init(config *config.GlobalConfig) {
-	f, err := os.OpenFile(config.App.Logfile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+func Init(config config.GlobalConfig) {
+	app := &App{
+		Config: config,
+	}
+	f, err := os.OpenFile(app.Config.App.Logfile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
-	logger = log.New(f, "\033[1;34m" + time.Now().Format(time.RFC822) + " :\033[0m ", 0)
+	app.Log = log.New(f, "\033[1;34m" + time.Now().Format(time.RFC822) + " :\033[0m ", 0)
 
-	for _, value := range config.Server {
-		logger.Printf(
+	for _, value := range app.Config.Server {
+		app.Log.Printf(
 			"Server %s => User:%s, IP Address: %s, Port: %s\n", value.Name, value.User, value.IpAddr, value.Port)
 	}
 }
